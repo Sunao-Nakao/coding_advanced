6cf0e6dbf44367863017ae94b774f4d72385e8e5
sunao nakao <sunawo.m.lg@gmail.com>
2024-05-21 12:22:52 +0900

    ヘッダーの編集

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
-  $("#hamburger-icon").on("click", function (e) {
-    e.preventDefault();
-    $(".toggle-list").slideToggle();
+    $("#hamburger-icon").on("click", function (e) {
+      e.preventDefault();
+      $(".toggle-list").slideToggle();
+    });
   });
-});
+  
