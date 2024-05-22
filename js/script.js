8637e9e870f6e0a0db9af7bfcc12ac5f64cc2f29
sunao nakao <sunawo.m.lg@gmail.com>
2024-05-22 13:10:46 +0900

    swiperを実装

--- js/script.js
+++ js/script.js
@@ -1,7 +1,24 @@
+// ハンバーガーメニュー
 $(document).ready(function () {
-    $("#hamburger-icon").on("click", function (e) {
-      e.preventDefault();
-      $(".toggle-list").slideToggle();
-    });
+  $("#hamburger-icon").on("click", function (e) {
+    e.preventDefault();
+    $(".toggle-list").slideToggle();
   });
-  
+});
+
+// スライドショー
+const swiper = new Swiper(".swiper", {
+  loop: true,
+  autoplay: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true, // クリック有効化
+  },
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
