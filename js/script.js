a14fd313b58885d580d40b5fde0ed4b935620ca0
sunao nakao <sunawo.m.lg@gmail.com>
2024-05-24 13:32:46 +0900

    レスポンシブデザインの実装

--- js/script.js
+++ js/script.js
@@ -14,6 +14,13 @@ const swiper = new Swiper(".swiper", {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+  slidesPerView: 1,
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+    },
+  },
+
   pagination: {
     el: ".swiper-pagination",
     clickable: true, // クリック有効化
